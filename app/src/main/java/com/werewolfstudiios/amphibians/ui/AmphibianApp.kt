b4f762c9d9fcab712631d134c7e951b1cffecf80
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2023-08-21 16:50:59 +0530

    theme and icons

--- app/src/main/java/com/werewolfstudiios/amphibians/ui/AmphibianApp.kt
+++ app/src/main/java/com/werewolfstudiios/amphibians/ui/AmphibianApp.kt
@@ -1,10 +1,14 @@
-@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class,
+    ExperimentalMaterial3Api::class
+)
 
 package com.werewolfstudiios.amphibians.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +18,14 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.werewolfstudiios.amphibians.R
 import com.werewolfstudiios.amphibians.ui.screens.AmphibianViewModel
@@ -34,10 +43,12 @@ fun AmphibianApp() {
                 .fillMaxSize()
                 .padding(it)
         ) {
-            val amphibianViewModel: AmphibianViewModel = viewModel(factory = AmphibianViewModel.Factory)
+            val amphibianViewModel: AmphibianViewModel =
+                viewModel(factory = AmphibianViewModel.Factory)
             HomeScreen(
                 amphibianUiState = amphibianViewModel.amphibianUiState,
-                retryAction = amphibianViewModel::getAmphibians)
+                retryAction = amphibianViewModel::getAmphibians
+            )
         }
     }
 }
@@ -47,11 +58,25 @@ fun AmphibianTopAppBar(scrollBehavior: TopAppBarScrollBehavior, modifier: Modifi
     TopAppBar(
         scrollBehavior = scrollBehavior,
         title = {
-            Text(
-                text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.headlineSmall,
-            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.ic_launcher_foreground),
+                    contentDescription = null,
+                    modifier = Modifier.size(64.dp).padding(0.dp),
+                    alignment = Alignment.Center
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.headlineSmall,
+                )
+            }
         },
         modifier = modifier
     )
+}
+
+@Preview
+@Composable
+fun AmphibianTopAppBarPreview() {
+    AmphibianTopAppBar(scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior())
 }
